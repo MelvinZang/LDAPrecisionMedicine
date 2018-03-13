c7441a93e0627c6093323da114d91bac5b2b4a1e
Zang <752087739@qq.com>
2018-03-13 15:59:31 +0800

    settings

--- src/main/java/lda/Lda.java
+++ src/main/java/lda/Lda.java
@@ -1,4 +1,28 @@
 package lda;
 
 public class Lda {
+
+    int nTopics;
+    int nIter = 2000;
+    double alpha = 0.1;
+    double eta = 0.01;
+    Integer randomState = null;
+    int refresh = 10;
+
+    Lda(int nTopics, int nIter, int alpha, int eta, Integer randomState, int refresh){
+        this.nTopics = nTopics;
+        this.nIter = nIter;
+        this.alpha = alpha;
+        this.eta = eta;
+        this.randomState = randomState;
+        this.refresh = refresh;
+
+        if (alpha <= 0 || eta <=0){
+            System.out.println("alpha and eta must be greater than zero");
+        }
+    }
+
+
+
+
 }
